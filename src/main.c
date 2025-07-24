3c35f8835b9af5e340f4bad8529adfa966f12c05
Maria Alejandra Castillo Martinez <m.alejandra.castillo.m@gmail.com>
2025-07-23 20:09:00 -0600

    feat(level1): add bitmap copy and comparison
    - Implemented save_bitmap_state() to clone bitmap
    - Added print_bitmap_comparison() to display before/after
    - Used textual representation ([x], [ ]) for visualization

---
+++ src/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define BITMAP_SIZE 128 
+int bitmap[BITMAP_SIZE] = {0};
+
+void print_bitmap_comparison(int old_bitmap[]){ 
+    printf("Old Bitmap\t\tNew Bitmap\n");
+    printf("\n         Before         \t\t          After\n");
+    for(int i = 0;  i < BITMAP_SIZE/8; i++){ 
+        // the old bitmap
+        for(int j = 0; j < 8; j++){
+            if(old_bitmap[(8*i)+j] == 0){
+                printf("[ ]"); 
+            }
+            else {
+                printf("[x]");
+            }
+        }
+        printf("\t\t"); 
+        // the updated bitmap
+        for(int j = 0; j < 8; j++){
+            if(bitmap[(8*i)+j] == 0){
+                printf("[ ]"); 
+            }
+            else {
+                printf("[x]");
+            }
+        }
+        printf("\n"); 
+    }
+}
+
+void save_bitmap_state(int copy_dest[]){
+    // copy de actual state of the bitmap
+    for (int i = 0; i < BITMAP_SIZE; i++) {
+        copy_dest[i] = bitmap[i];
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int old_bitmap[BITMAP_SIZE]; 
+    save_bitmap_state(old_bitmap);  
+
+    // update the bitmap
+    for(int i = 0; i < BITMAP_SIZE/8; i++){
+        bitmap[(i*8)+4]=1;
+        bitmap[i*8]=1;
+    }; 
+    
+    print_bitmap_comparison(old_bitmap);
+    return 0;
+}
