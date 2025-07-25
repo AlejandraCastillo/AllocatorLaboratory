ee8bf53b065a3e9ec61bf50f3a51d68dd7ca423d
Maria Alejandra Castillo Martinez <m.alejandra.castillo.m@gmail.com>
2025-07-24 19:57:43 -0600

    feat(level1): add allocate_block function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
 #define BITMAP_SIZE 128 
-int bitmap[BITMAP_SIZE] = {0};
+int bitmap[BITMAP_SIZE] = {
+    1,1,0,1,1,1,0,0, //1, 2
+    1,0,0,0,0,0,0,1, //6
+    1,1,0,0,0,1,1,1, //3
+    0,1,1,0,0,0,1,1, //3 
+    1,1,1,0,0,0,0,0, //5
+    1,0,0,0,0,0,0,1, //6
+    1,1,1,1,1,0,0,0,
+    0,0,1,1,1,1,1,0, //5, 
+    0,0,0,0,0,0,0,1, //8
+    1,1,0,0,0,1,1,1, //3
+    0,1,1,0,0,0,1,1, //3 
+    1,1,1,0,0,0,0,0, //5
+    1,0,0,0,0,0,0,1, //6
+    1,1,1,1,1,0,0,0,
+    0,0,1,1,1,1,1,0, //5, 1
+};
 
 void print_bitmap_comparison(int old_bitmap[]){ 
     // headers 
@@ -39,18 +55,43 @@ void save_bitmap_state(int copy_dest[]){
     }
 }
 
+int allocate_block(int block_size){
+    for(int i = 0; i < BITMAP_SIZE; i++){ //recorremos el bitmap
+        if(bitmap[i] == 0){ //hasta encontrar un 0
+            for(int j = 0; j < block_size; j++){ //entonces contamos ceros hasta que...
+                if(bitmap[i+j] != 0){ //encontremos una celda ocupada
+                    i += j; //entonces actualizamos i
+                    j = block_size; //y salimos de este for 
+                }
+                else if (j == block_size-1){ // o hasta que tengamos un bloque del tamaño correcto
+                    for(int k = 0; k < block_size; k++){ //entoences nos regresamos al incio del bloque
+                        bitmap[i+k] =1; //y lo ocupamos con 1s
+                    }
+                    return i; //y devolvemos la direccion del bloque 
+                }
+            } 
+        }
+    }
+    return -1; //no hay espacio disponible 
+}
+
 
 int main(int argc, char const *argv[])
 {
     int old_bitmap[BITMAP_SIZE]; 
     save_bitmap_state(old_bitmap);  
 
-    // update the bitmap
-    for(int i = 0; i < BITMAP_SIZE/8; i++){
-        bitmap[(i*8)+4]=1;
-        bitmap[i*8]=1;
-    }; 
+    // prepare bitmap for testing
+    int block_size = 7;
+    int dir = allocate_block(block_size);
     
-    print_bitmap_comparison(old_bitmap);
+    if(dir != -1){
+        printf("\n\nblockSize = %d \ndir = %d\n\n", block_size, dir);
+        print_bitmap_comparison(old_bitmap);    
+    }
+    else {
+        printf("Lo siento, no hay espacio en memoria");
+    }
+
     return 0;
 }
